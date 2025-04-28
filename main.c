96902d057e84266b51dd5122722689d78ac32e99
spyhere <gekav3@mail.ru>
2025-04-28 18:46:29 +0300

    Add todo for this project

--- main.c
+++ main.c
@@ -9,6 +9,18 @@ int main() {
   Array array = new_array(1,4,3,4,0);
   char *array_joined = array_join(&array, "-**-");
   printf("My array is %s\n", array_joined);
+  // TODO:
+  // 1. Make array_join to work with digits bigger than 0-9
+  // 2. array_foreach
+  // 3. array_filter
+  // 4. array_find
+  // 5. array_map
+  // 6. array_push
+  // 7. array_pop
+  // 8. array_shift
+  // 9. array_unshift
+  // 10. array_sort
+  // 11??? splice/slice
   return 0;
 }
 
